6cbc43852c05719622f8831cd696c934484e8eb6
macana-del <macanagarciaglenaimar@gmail.com>
2026-02-26 19:54:13 -0500

    Create macana-del.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/macana-del.py
@@ -0,0 +1,36 @@
+# https://python.org
+
+# Comentario en una linea
+
+"""
+Esto también 
+es un comentario 
+de varias lineas
+"""
+
+'''
+Esto también 
+es un comentario 
+de varias lineas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante"  # por convencion
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("¡Hola, Python!")
+
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string)
+
